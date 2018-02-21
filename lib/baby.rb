4fd4f489974a9ca068aad9986b74c2ad37c73a1b
Robert W <robwash@gmail.com>
2018-02-21 20:56:50 +0000

    automatically backed up by learn

--- lib/baby.rb
+++ lib/baby.rb
@@ -1,9 +1,26 @@
+# We want to keep track of all babies that are born:
+
+#1. Where can we do that?
+  # -  The only place we can hook into a baby being born is initialize
+#2. Whos responsibility is it?
+
 class Baby
   attr_accessor :name
 
   def initialize(name = nil)
     @name = name
     cry
+    # Save the baby.  Put it somewhere that won't get lost. 
+      # 3. Where do we put that data?
+      @all << ?
+      # @all belongs to a particular baby
+      # it should not be a particular baby's responsibility to remember
+      # all the babies
+
+      # It's the classes responsibility
+      # What's a variable that operates on the class scope as opposed to the 
+      # instance
+      # How does a baby refer to itsself?
   end
 
   def cry
