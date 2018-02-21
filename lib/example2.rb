5490cf889911b8038ae9f8e7a92ab96607789870
Robert W <robwash@gmail.com>
2018-02-21 18:29:21 +0000

    automatically backed up by learn

--- lib/example2.rb
+++ lib/example2.rb
@@ -1,9 +1,14 @@
 class Dog
   attr_reader :name
 
-  def initialize(name)  # => Hook / Callback / Life Cycle Event
+  def initialize(name)  # => Hook / Callback / Life Cycle Eventg
     puts "A new dog was just born!!!"
     @name = name
+    @born_on = Time.now
+  end
+
+  def age
+    Time.now - @born_on
   end
 
 end
